6d568ac2b855fadce6796c3b67b99ed3cb8e35e5
Тимур Воробьев <misikon@mail.ru>
2021-07-19 16:04:54 +0300

    added controllers

---
+++ src/main/java/api/response/InitResponse.java
@@ -0,0 +1,14 @@
+package api.response;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitResponse {
+    private String title;
+    private String subtitle;
+    private String phone;
+    private String email;
+    private String copyright;
+    private String copyrightForm;
+
+}
--- src/main/java/controller/DefaultController.java
+++ src/main/java/controller/DefaultController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import api.response.InitResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class DefaultController {
+
+    @RequestMapping("/")
+    private String index(Model model) {
+        return "index";
+    }
+
 }
