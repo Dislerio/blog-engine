ac2c1062654d77b6afc302d082d3e40d8a645664
Тимур Воробьев <misikon@mail.ru>
2021-07-19 17:25:57 +0300

    tried changing pom dependencies

--- src/main/java/controller/DefaultController.java
+++ src/main/java/controller/DefaultController.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class DefaultController {
 
     @RequestMapping("/")
     private String index(Model model) {
         return "index";
+
     }
 
 }
